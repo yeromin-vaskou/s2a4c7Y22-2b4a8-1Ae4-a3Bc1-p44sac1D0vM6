664601c44d07b92f26cf5e4deab2e012982e48d1
Gleb Yeromin <vaskou221@gmail.com>
2023-01-29 19:20:22 +0200

    finished simple db isnsert operation and routing

--- main.go
+++ main.go
@@ -1,17 +1,16 @@
 package main
 
 import (
-	// "context"
-	// "encoding/json"
+	"context"
 	"encoding/json"
 	"fmt"
 	"net/http"
 	"time"
 
+	"github.com/gin-gonic/gin"
 	"github.com/thedevsaddam/gojsonq/v2"
-	// "go.mongodb.org/mongo-driver/bson"
-	// "go.mongodb.org/mongo-driver/mongo"
-	// "go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 const uri = "mongodb://localhost:27017/"
@@ -71,34 +70,46 @@ func GetJson(data interface{}) string {
 	return string(val)
 }
 
-func GetRecoveredAmount(url string) {
-	var General General
-	err := GetDecode(url, &General)
-	if err != nil {
-		fmt.Println("err: ", err)
-	} else {
-		fmt.Printf("Here is how many people recovered from Covid: %d!\n", General.Global.TotalRecovered)
-	}
-}
-
-func GetCountryByCode(obj interface{}, code string) {
-	var res string = GetJson(&obj)
-	jq := gojsonq.New().FromString(res)
-
-	cool_json := jq.From("Countries").Where("CountryCode", "=", code).Get()
-
-	b, err := json.MarshalIndent(cool_json, "", "  ")
-	if err != nil {
-		fmt.Println("marshal err: ", err)
-	}
-
-	fmt.Println(string(b))
-}
-
 func main() {
+	r := gin.Default()
 	client = &http.Client{Timeout: 10 * time.Second}
-	var general General
+	mongo_client, err := mongo.NewClient(options.Client().ApplyURI(uri))
+	if err != nil {
+		panic(err)
+	}
 
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err = mongo_client.Connect(ctx)
+	if err != nil {
+		panic(err)
+	}
+
+	coll := mongo_client.Database("pro-ect").Collection("covid")
+	var general General
 	GetDecode(url, &general)
-	GetCountryByCode(&general, "DZ")
+	doc := &general
+
+	result, err := coll.InsertOne(context.TODO(), doc)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Inserted: %v\n", result.InsertedID)
+
+	r.GET("/country/:code", func(c *gin.Context) {
+		var res string = GetJson(&general)
+		jq := gojsonq.New().FromString(res)
+		var code = c.Params.ByName("code")
+
+		cool_json := jq.From("Countries").Where("CountryCode", "=", code).Get()
+
+		b, err := json.MarshalIndent(cool_json, "", "   ")
+		if err != nil {
+			fmt.Println("marshal err: ", err)
+		}
+
+		c.JSON(200, string(b))
+	})
+
+	r.Run()
 }
--- tempCodeRunnerFile.go
+++
@@ -1,4 +0,0 @@
-var res string = GetJson(&general) // general is a struct
-	jq := gojsonq.New().FromString(res) 
-
-	cool_json := jq.From("Countries").Where("CountryCode", "=", "AF").Get()
